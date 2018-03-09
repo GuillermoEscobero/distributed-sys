edcd1cc175510f0e519083628d67b538734b6b0f
Guillermo Escobero <100346060@alumnos.uc3m.es>
2018-03-09 12:09:23 +0100

    Ready

--- Makefile
+++ Makefile
@@ -3,9 +3,6 @@ all:
 	gcc -g -Wall -c keys.c -lrt
 	ar -rv libkeys.a keys.o
 	gcc -g -Wall -o client client.c libkeys.a -lrt
-	gcc -g -Wall -c keys2.c -lrt
-	ar -rv libkeys2.a keys2.o
-	gcc -g -Wall -o client2 client2.c libkeys2.a -lrt
 
 clean:
-	rm -f server client client2 keys.o keys2.o libkeys.a libkeys2.a
+	rm -f server client keys.o libkeys.a
--- client.c
+++ client.c
@@ -5,8 +5,6 @@
  * Author: Guillermo Escobero
  ****************************************************************************/
 
-#include <unistd.h>
-
 #include <stdio.h>
 #include <string.h>
 #include "keys.h"
@@ -21,19 +19,22 @@ int main(void) {
     printf("Done\n");
 
     printf("\n********** TEST2: SET_VALUE ************\n");
-    printf("Code expected: 0, got: %d\n", set_value(3, "testid3\0", 3.33));
+    printf("Code expected: 0, got: %d\n", set_value(3, "testid3", 3.33));
     printf("Done\n");
 
 
-    printf("********** TEST3: set key already used ************\n");
-    printf("Code expected: -1, got: %d\n", set_value(3, "SSSSSS3\0", 321.33));
+    printf("\n********** TEST3: set key already used ************\n");
+    printf("Code expected: -1, got: %d\n", set_value(3, "SSSSSS3", 321.33));
 
     printf("\n********** TEST4: GET_VALUE ************\n");
     printf("Code expected: 0, got: %d\n", get_value(3, res1, &res2));
     printf("Expected: testid3 (value1) from server: %s\n", res1);
     printf("Expected: 3.33 (value2) from server: %f\n", res2);
 
-    strcpy(res1, "modifiedid3\0");
+    printf("\n********** TEST4: GET_VALUE (no existing key) ************\n");
+    printf("Code expected: -1, got: %d\n", get_value(89, res1, &res2));
+
+    strcpy(res1, "modifiedid3");
     res2 = 4.44;
 
     printf("\n********** TEST5: MODIFY_VALUE ************\n");
@@ -41,33 +42,32 @@ int main(void) {
 
     strcpy(res1, "clean\0");
     res2 = 0.00;
-    
+
     printf("\n********** TEST6: check test 5 ************\n");
     printf("Code expected: 0, got: %d\n", get_value(3, res1, &res2));
-    
+
     printf("Expected: modifiedid3 (value1) from server: %s\n", res1);
     printf("Expected: 4.44 (value2) from server: %f\n", res2);
 
     printf("\n********** TEST7: NUM_ITEMS ************\n");
-    printf("Add another triplet -> Code expected: 0, got: %d\n", set_value(56, "testid56\0", 57.75));
-    printf("Add another triplet -> Code expected: 0, got: %d\n", set_value(372, "testid372\0", 372.43));
-
+    printf("Add another triplet -> Code expected: 0, got: %d\n", set_value(56, "testid56", 57.75));
+    printf("Add another triplet -> Code expected: 0, got: %d\n", set_value(372, "testid372", 372.43));
 
     printf("Items expected: 3, sent by server: %d\n", num_items());
-printf("SLEEPING 20''"); 
-sleep(20);
-    printf("Code expected: 0, got: %d\n", get_value(123, res1, &res2));
-    printf("Expected: GABOLO 123.123123 from server: %s, %f\n", res1, res2);
 
-    printf("\n********** TEST4: GET_VALUE ************\n");
-    printf("Code expected: 0, got: %d\n", get_value(56, res1, &res2));
-    printf("Expected: testid56 (value1) from server: %s\n", res1);
-    printf("Expected: 57.75 (value2) from server: %f\n", res2);
 
-    printf("\n********** TEST7: DELETE_KEY ************\n");
+    printf("\n********** TEST8: DELETE_KEY ************\n");
+    printf("Deleting key 3\n");
     printf("Code expected: 0, got: %d\n", delete_key(3));
-    printf("Try to delete again: Code expected: -1, got: %d\n", delete_key(3));
+    printf("Try to delete again key 3: Code expected: -1, got: %d\n", delete_key(3));
     printf("Items expected: 2, sent by server: %d\n", num_items());
 
+    printf("\n********** TEST9: SET_VALUE deleted ************\n");
+    printf("Code expected: 0, got: %d\n", set_value(3, "new3", 393.12));
+
+    printf("\n********** TEST10: check test 9 ************\n");
+    printf("Code expected: 0, got: %d\n", get_value(3, res1, &res2));
+    printf("Expected: new3 (value1) from server: %s\n", res1);
+    printf("Expected: 393.12 (value2) from server: %f\n", res2);
 
 }
